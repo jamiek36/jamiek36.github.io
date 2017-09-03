38e08c303f4693c2a28d580fc0c5125e026e74c2
jamiek36 <jkeene@geomatic.com.au>
2017-09-03 22:25:28 +1000

    Add files via upload

--- build/aframe-ar.js
+++ build/aframe-ar.js
@@ -6613,25 +6613,7 @@ ARjs.SessionDebugUI = function(arSession, tangoPointCloud){
 	// this.domElement.appendChild(domElement)
 	// domElement.innerHTML = 'AR.js Session Debug'
 
-	//////////////////////////////////////////////////////////////////////////////
-	//		current-tracking-backend
-	//////////////////////////////////////////////////////////////////////////////
-
-	var domElement = document.createElement('span')
-	domElement.style.display = 'block'
-	this.domElement.appendChild(domElement)
-	domElement.innerHTML = '<b>trackingBackend</b> : ' +trackingBackend
 	
-	//////////////////////////////////////////////////////////////////////////////
-	//		augmented-websites
-	//////////////////////////////////////////////////////////////////////////////
-	var domElement = document.createElement('a')
-	domElement.innerHTML = 'Share on augmented-websites'
-	domElement.style.display = 'block'
-	domElement.setAttribute('target', '_blank')
-	domElement.href = 'https://webxr.io/augmented-website?'+location.href
-	this.domElement.appendChild(domElement)				
-
 	//////////////////////////////////////////////////////////////////////////////
 	//		toggle-point-cloud
 	//////////////////////////////////////////////////////////////////////////////
@@ -6690,72 +6672,6 @@ ARjs.AnchorDebugUI = function(arAnchor){
 	// this.domElement.appendChild(domElement)
 	// domElement.innerHTML = 'Anchor Marker Debug'
 
-	//////////////////////////////////////////////////////////////////////////////
-	//		current-tracking-backend
-	//////////////////////////////////////////////////////////////////////////////
-
-	var domElement = document.createElement('span')
-	domElement.style.display = 'block'
-	this.domElement.appendChild(domElement)
-	domElement.innerHTML = '<b>markersAreaEnabled</b> :' +arAnchor.parameters.markersAreaEnabled
-
-	//////////////////////////////////////////////////////////////////////////////
-	//		toggle-marker-helper
-	//////////////////////////////////////////////////////////////////////////////
-
-	if( arAnchor.parameters.markersAreaEnabled ){
-		var domElement = document.createElement('button')
-		domElement.style.display = 'block'
-		this.domElement.appendChild(domElement)
-
-		domElement.id= 'buttonToggleMarkerHelpers'
-		domElement.innerHTML = 'toggle-marker-helper'
-		domElement.href='javascript:void(0)'
-
-		var subMarkerHelpersVisible = false
-		domElement.addEventListener('click', function(){
-			subMarkerHelpersVisible = subMarkerHelpersVisible ? false : true
-			arAnchor.markersArea.setSubMarkersVisibility(subMarkerHelpersVisible)		
-		})
-	}
-	
-	//////////////////////////////////////////////////////////////////////////////
-	//		Learn-new-marker-area
-	//////////////////////////////////////////////////////////////////////////////
-
-	if( arAnchor.parameters.markersAreaEnabled ){
-		var domElement = document.createElement('button')
-		domElement.style.display = 'block'
-		this.domElement.appendChild(domElement)
-
-		domElement.id = 'buttonMarkersAreaLearner'
-		domElement.innerHTML = 'Learn-new-marker-area'
-		domElement.href ='javascript:void(0)'
-
-		domElement.addEventListener('click', function(){
-			var learnerBaseURL = ARjs.Context.baseURL + 'examples/multi-markers/examples/learner.html'
-			ARjs.MarkersAreaUtils.navigateToLearnerPage(learnerBaseURL, trackingBackend)
-		})	
-	}
-
-	//////////////////////////////////////////////////////////////////////////////
-	//		Reset-marker-area
-	//////////////////////////////////////////////////////////////////////////////
-
-	if( arAnchor.parameters.markersAreaEnabled ){
-		var domElement = document.createElement('button')
-		domElement.style.display = 'block'
-		this.domElement.appendChild(domElement)
-
-		domElement.id = 'buttonMarkersAreaReset'
-		domElement.innerHTML = 'Reset-marker-area'
-		domElement.href ='javascript:void(0)'
-
-		domElement.addEventListener('click', function(){
-			ARjs.MarkersAreaUtils.storeDefaultMultiMarkerFile(trackingBackend)
-			location.reload()
-		})
-	}
 }
 // @namespace
 var ARjs = ARjs || {}
